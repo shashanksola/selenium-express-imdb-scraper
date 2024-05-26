7929c40428be3707587db1de4fc3ae8fbdb06830
shashanksola <solashashank1010@gmail.com>
2024-05-26 13:18:15 +0530

    initital commit

---
+++ scraper.py
@@ -0,0 +1,38 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
+import sys
+
+driver = webdriver.Chrome()
+req = str(sys.argv[1])
+# req = "avengers"
+driver.get("https://www.imdb.com/find/?q=" + req + "&ref_=nv_sr_sm")
+
+movies = driver.find_elements(
+    By.CLASS_NAME, "ipc-metadata-list-summary-item__t")
+
+top_related_movies = list()
+for movie in movies:
+    link_to_movie = movie.get_attribute("href")
+    movie_name = movie.text
+    if "title" in link_to_movie:
+        plot_link = link_to_movie[:36] + "/plotsummary/?ref_=tt_stry_pl"
+
+        top_related_movies.append((movie_name, plot_link))
+
+# print(top_related_movies)
+
+movie_short_plots = list()
+movie_long_plots = list()
+
+for movie, movie_plot_link in top_related_movies:
+    driver.get(movie_plot_link)
+
+    summaries = driver.find_elements(By.CLASS_NAME, "ipc-metadata-list__item")
+    movie_short_plots.append((movie, summaries[0].text))
+    movie_long_plots.append((movie, summaries[1].text))
+
+print(movie_short_plots)
---
+++ server.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const { spawn } = require('child_process');
+
+let global = ["head"];
+
+const getMovieSummary = (movieName) => {
+    return new Promise((resolve, reject) => {
+        const scraper = spawn('python', ['scraper.py', movieName]);
+
+        let dataString = '';
+
+        scraper.stdout.on('data', (data) => {
+            dataString += data.toString();
+        });
+
+        scraper.stderr.on('data', (data) => {
+            console.error(`stderr: ${data}`);
+            reject(data.toString());
+        });
+
+        scraper.on('close', (code) => {
+            if (code !== 0) {
+                reject(`Process exited with code ${code}`);
+            } else {
+                resolve(dataString);
+            }
+        });
+    });
+};
+
+const app = express();
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.sendFile();
+})
+
+app.get('/movies/:movieName', async (req, res) => {
+    try {
+        const summary = await getMovieSummary(req.params.movieName);
+        res.send(summary);
+    } catch (error) {
+        res.status(500).send(`Error: ${error}`);
+    }
+})
+
+app.listen(3000, () => {
+    console.log("Server is running at http://localhost:3000");
+})
+
+module.exports = app;
